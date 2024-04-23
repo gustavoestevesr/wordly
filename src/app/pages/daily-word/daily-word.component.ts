67b16d8f1623998d4c7873cd90ef74f6c4b52ede
gustavoestevesr <gustavoesteves@directon.com.br>
2024-04-22 23:42:17 -0300

    feat: styling the daily-word

--- src/app/pages/daily-word/daily-word.component.ts
+++ src/app/pages/daily-word/daily-word.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-daily-word',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule, MatIconModule],
   templateUrl: './daily-word.component.html',
   styleUrl: './daily-word.component.scss'
 })
 export class DailyWordComponent {
-
+  public qtdLetters: number = 4;
+  public router = inject(Router);
 }
